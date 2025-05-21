a6440a2d779eb18a72c31dfa3193ff246fca1644
mhaowork <mhaowork@users.noreply.github.com>
2025-05-20 20:21:39 -0700

    fix lint

--- lib/platforms/types.ts
+++ lib/platforms/types.ts
@@ -1,5 +1,6 @@
 import { Device } from '../../types/device';
 
 export interface PlatformConnector {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   fetchDevices(credentials: any): Promise<Device[]>;
 } 

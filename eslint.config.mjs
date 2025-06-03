a31d0f03bf8262e03159812b3416c0735b6f0ba0
mhaowork <mhaowork@users.noreply.github.com>
2025-06-03 10:35:05 -0700

    add youMightNotNeedAnEffect lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import youMightNotNeedAnEffect from "eslint-plugin-react-you-might-not-need-an-effect";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,24 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // "no-console": "warn",
+      "prefer-const": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      // "react/jsx-uses-react": "off",
+      // "react/react-in-jsx-scope": "off",
+    }
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "react-you-might-not-need-an-effect": youMightNotNeedAnEffect,
+    },
+    rules: {
+      "react-you-might-not-need-an-effect/you-might-not-need-an-effect": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
